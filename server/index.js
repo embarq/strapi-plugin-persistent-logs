624e3daecc37c22bc4ddcb336b78e8f91de1ecf1
embarq <embarq.dev@gmail.com>
2023-12-13 15:36:58 +0900

    remove routes and stuff

--- server/index.js
+++ server/index.js
@@ -6,9 +6,6 @@ const destroy = require('./destroy');
 const config = require('./config');
 const contentTypes = require('./content-types');
 const controllers = require('./controllers');
-const routes = require('./routes');
-const middlewares = require('./middlewares');
-const policies = require('./policies');
 const services = require('./services');
 
 module.exports = {
